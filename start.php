3acfb19e506d392e3e24a7f4e727ca885cbe222c
Aurélien Maille <bobe+git@webnaute.net>
2005-11-14 20:24:14 +0000

    *** empty log message ***

--- start.php
+++ start.php
@@ -118,28 +118,17 @@ if( !(time() % 10) || !defined('IN_ADMIN') )
 $tmp_name = 'tmp';
 
 //
-// Utilisons de préférence le répertoire tmp du système
+// Si nous avons un accès restreint à cause d'open_basedir sur le serveur, 
+// nous devrons utiliser le dossier des fichiers temporaires du script 
 //
-if( !OPEN_BASEDIR_RESTRICTION && is_readable('/tmp') && is_writable('/tmp') )
+$tmp_name = trim($tmp_name, '/');
+
+if( OPEN_BASEDIR_RESTRICTION && !is_writable(WA_ROOTDIR . '/' . $tmp_name) )
 {
-	$tmpdir = '/tmp';
-}
-else
-{
-	//
-	// Si nous avons un accès restreint à cause d'open_basedir sur le serveur,
-	// ou que le répertoire tmp du système est inaccessible, nous devrons
-	// utiliser le répertoire tmp du script.
-	//
-	$tmpdir = WA_ROOTDIR . '/' . trim($tmp_name, '/');
-	
-	if( !is_writable($tmpdir) )
-	{
-		trigger_error('tmp_dir_not_writable', MESSAGE);
-	}
+	trigger_error('tmp_dir_not_writable', MESSAGE);
 }
 
-define('WA_TMPDIR',    $tmpdir);
+define('WA_TMPDIR',    WA_ROOTDIR . '/' . $tmp_name, true);
 define('WAMAILER_DIR', WA_ROOTDIR . '/includes/wamailer');
 
 ?>
