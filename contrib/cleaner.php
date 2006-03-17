6274a03e100385da14864a51122f25b75e97b8a1
Aurélien Maille <bobe+git@webnaute.net>
2006-03-17 18:29:43 +0000

    Ajout option de synchronisation entre le répertoire des fichiers joints et les tables du script

--- contrib/cleaner.php
+++ contrib/cleaner.php
@@ -24,8 +24,11 @@
  * @license http://www.gnu.org/copyleft/gpl.html  GNU General Public License
  * @version $Id$
  * 
- * Recherche les entrées orphelines dans les tables abonnes et abo_liste
+ * - Recherche les entrées orphelines dans les tables abonnes et abo_liste
  * et les efface, si demandé.
+ * - Peut également effacer les fichiers présents dans le répertoire d'upload
+ * sans correspondance dans les tables de fichiers joints ainsi que les entrées
+ * dans les tables de fichiers pour lesquelles le fichier n'est plus présent
  */
 
 //
@@ -41,16 +44,28 @@ define('WA_ROOTDIR',   '..');
 
 require WA_ROOTDIR . '/start.php';
 
+load_settings();
+
 $type = ( !empty($_GET['type']) ) ? $_GET['type'] : '';
 
-if( $type != 'files' && $type != 'subscribers' )
+if( $type != 'files' && $type != 'files2' && $type != 'subscribers' )
 {
     $output->basic(
-        '<h1>Selection :</h1>
-<ul>
-    <li> <a href="cleaner.php?type=files">Fichiers joints</a> </li>
-    <li> <a href="cleaner.php?type=subscribers">Abonnés</a> </li>
-</ul>');
+        '<h1>Outils&#160;:</h1>
+
+<dl>
+	<dt><a href="cleaner.php?type=files">Fichiers joints</a></dt>
+	<dd>Supprime les entrées de la table wa_joined_files qui n\'ont pas de correspondance
+	dans la table wa_log_files, et vice versa.</dd>
+	<dt><a href="cleaner.php?type=files2">Fichiers joints (2)</a></dt>
+	<dd>Supprime les entrées des tables wa_joined_files et wa_log_files si le fichier concerné
+	n\'est plus présent dans le répertoire des fichiers joints, et vice versa</dd>
+	<dt><a href="cleaner.php?type=subscribers">Abonnés</a></dt>
+	<dd>Supprime les entrées de la table wa_abonnes qui n\'ont pas de correspondance
+	dans la table wa_abo_liste, et vice versa.</dd>
+</dl>
+
+<p>Le script affiche le nombre d\'entrées concernées et demande confirmation avant de les supprimer.</p>');
 }
 
 if( $type == 'subscribers' )
@@ -186,13 +201,81 @@ else if( $type == 'files' )
     }
     
     $data  = '<ul>';
-    $data .= '<li>' . $total_diff_1 . ' entrées orphelines dans la table ' . JOINED_FILES_TABLE . ' (' . implode(', ', $diff_1) . ')</li>';
-    $data .= '<li>' . $total_diff_2 . ' entrées orphelines dans la table ' . LOG_FILES_TABLE . ' (' . implode(', ', $diff_2) . ')</li>';
+    $data .= '<li>' . $total_diff_1 . ' entrées orphelines dans la table ' . JOINED_FILES_TABLE . ' (ids: ' . implode(', ', $diff_1) . ')</li>';
+    $data .= '<li>' . $total_diff_2 . ' entrées orphelines dans la table ' . LOG_FILES_TABLE . ' (ids: ' . implode(', ', $diff_2) . ')</li>';
     $data .= '</ul>';
     
     if( $total_diff_1 > 0 || $total_diff_2 > 0 )
     {
         $data .= '<p><a href="cleaner.php?type=files&amp;delete=true">Effacer les entrées orphelines</p>';
+    }
+    
+    $output->basic($data);
+}
+else if( $type == 'files2' )
+{
+	$sql = "SELECT file_id, file_physical_name
+		FROM " . JOINED_FILES_TABLE;
+	if( !($result = $db->query($sql)) )
+    {
+        trigger_error('Impossible d\'obtenir les identifiants [joined_files]', CRITICAL_ERROR);
+    }
+	
+	$upload_path    = WA_ROOTDIR . '/' . $nl_config['upload_path'];
+	$sql_delete_ids = array();
+	$joined_files   = array();
+	$delete_files   = array();
+	while( $row = $result->fetch() )
+	{
+		if( !file_exists($upload_path . $row['file_physical_name']) ) {
+			array_push($sql_delete_ids, $row['file_id']);
+		}
+		
+		array_push($joined_files, $row['file_physical_name']);
+	}
+	
+	$browse = dir($upload_path);
+	while( ($entry = $browse->read()) !== false )
+	{
+		if( is_file($upload_path . $entry) && $entry != 'index.html' && !in_array($entry, $joined_files) )
+		{
+			array_push($delete_files, $entry);
+			
+			if( !empty($_GET['delete']) )
+			{
+				unlink($upload_path . $entry);
+			}
+		}
+	}
+	
+	if( !empty($_GET['delete']) )
+    {
+		if( count($sql_delete_ids) > 0 )
+		{
+			$db->beginTransaction();
+			
+			$sql = "DELETE FROM " . JOINED_FILES_TABLE . " 
+				WHERE file_id IN(" . implode(', ', $sql_delete_ids) . ")";
+			$db->query($sql);
+			
+			$sql = "DELETE FROM " . LOG_FILES_TABLE . " 
+				WHERE file_id IN(" . implode(', ', $sql_delete_ids) . ")";
+			$db->query($sql);
+			
+			$db->commit();
+		}
+        
+        $output->basic('Opération effectuée');
+    }
+    
+    $data  = '<ul>';
+    $data .= '<li>' . count($sql_delete_ids) . ' fichiers manquants (ids: ' . implode(', ', $sql_delete_ids) . ')</li>';
+    $data .= '<li>' . count($delete_files) . ' fichiers dans "' . $nl_config['upload_path'] . '" sans entrée correspondante dans la base de données (fichiers: ' . implode(', ', $delete_files) . ')</li>';
+    $data .= '</ul>';
+    
+    if( count($sql_delete_ids) > 0 || count($delete_files) > 0 )
+    {
+        $data .= '<p><a href="cleaner.php?type=files2&amp;delete=true">Effacer les entrées orphelines</p>';
     }
     
     $output->basic($data);
