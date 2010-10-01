4cb89dd6bae3f8c6b55b6071730ad4811c76da77
Aurélien Maille <bobe+git@webnaute.net>
2010-10-01 22:52:23 +0000

    Ajout d’un squelette de fichier de configuration (cas de fichier de config égaré/supprimé par erreur)

---
+++ includes/config.sample.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+//
+// Utilisez ce fichier si vous souhaitez (re)créer un fichier de configuration
+// valide sans utiliser le script d'installation de Wanewsletter.
+//
+define('NL_INSTALLED', true);
+define('WA_VERSION',   '2.3-dev');
+
+// exemple de DSN pour SQLite 3
+//$dsn = 'sqlite:/home/to/my/htdocs/wanewsletter/includes/sql/wanewsletter.sqlite3';
+// exemple de DSN pour MySQL
+//$dsn = 'mysql://username:password@localhost/dbname?charset=latin1';
+$prefixe = 'wa_';
+
