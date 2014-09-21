951eee715e435edcedb19bacf04179b542004311
Aurélien Maille <bobe+git@webnaute.net>
2014-09-21 14:30:01 +0200

    Les magic quotes ont été supprimés dans PHP 5.4
    Évitera des problèmes le jour où `get_magic_quotes_gpc()` est supprimée

--- start.php
+++ start.php
@@ -78,18 +78,24 @@ else
 }
 
 //
-// Désactivation de magic_quotes_runtime + 
-// magic_quotes_gpc et retrait éventuel des backslashes 
+// Les guillemets magiques ont été supprimés dans PHP 5.4.0
 //
-@ini_set('magic_quotes_runtime', 0);
-
-if( get_magic_quotes_gpc() )
+if( version_compare(PHP_VERSION, '5.4.0', '<') )
 {
-	strip_magic_quotes_gpc($_GET);
-	strip_magic_quotes_gpc($_POST);
-	strip_magic_quotes_gpc($_COOKIE);
-	strip_magic_quotes_gpc($_FILES, true);
-	strip_magic_quotes_gpc($_REQUEST);
+	//
+	// Désactivation de magic_quotes_runtime +
+	// magic_quotes_gpc et retrait éventuel des backslashes
+	//
+	@ini_set('magic_quotes_runtime', 0);
+
+	if( get_magic_quotes_gpc() )
+	{
+		strip_magic_quotes_gpc($_GET);
+		strip_magic_quotes_gpc($_POST);
+		strip_magic_quotes_gpc($_COOKIE);
+		strip_magic_quotes_gpc($_FILES, true);
+		strip_magic_quotes_gpc($_REQUEST);
+	}
 }
 
 //
