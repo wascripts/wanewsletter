ed471bea7d58063c2b3a7e5f8a78527dfa822b80
Aurélien Maille <bobe+git@webnaute.net>
2014-10-18 13:07:00 +0200

    Ajout du système d'exploitation dans les infos de débogage
    Cette information peut ne pas être présente dans la variable SERVER_SOFTWARE

--- admin/tools.php
+++ admin/tools.php
@@ -362,7 +362,7 @@ switch( $mode )
 		
 		list($infos) = parseDSN($dsn);
 
-		wan_print_row('Type de serveur', $_SERVER['SERVER_SOFTWARE']);
+		wan_print_row('Type de serveur', $_SERVER['SERVER_SOFTWARE'] . ' - ' . PHP_OS);
 		wan_print_row('Connexion sécurisée', wan_ssl_connection() ? 'oui' : 'non');
 
 		if( $db->engine == 'sqlite' ) {
